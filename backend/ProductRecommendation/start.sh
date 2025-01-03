d9dfd3dc0f898e6f06a82fbe14211ee59b27f987
dawidolko <dawid_olko@outlook.com>
2025-01-03 21:33:04 +0100

    expansion of the database

--- backend/ProductRecommendation/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Aktywacja środowiska wirtualnego (jeśli używasz)
-if [ -f env/bin/activate ]; then
-    source env/bin/activate
-fi
-
-# Uruchomienie serwera Django
-python manage.py runserver
